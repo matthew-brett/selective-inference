f98b008ce4d32d195a9ab51cf81f773bdbad8999
Jelena Markovic <jelenam@stanford.edu>
2017-01-13 00:04:33 -0800

    plotting the coverage

---
+++ selection/approx_ci/tests/plot_coverage.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from selection.approx_ci.tests.test_glm import test_glm
+
+
+def compute_coverage():
+    n, p, s = 500, 100, 5
+    snr_grid = np.linspace(0, 10, num=5)
+    snr_grid_length = snr_grid.shape[0]
+    niter = 50
+    lasso_selective_xaxis = []
+    lasso_naive_xaxis = []
+    logistic_selective_xaxis = []
+    logistic_naive_xaxis = []
+
+    for i in range(snr_grid_length):
+        lasso_selective = []
+        lasso_naive = []
+        logistic_selective = []
+        logistic_naive = []
+        for _ in range(niter):
+            result_lasso = test_glm(n=n, p=p, s=s, snr = snr_grid[i], loss='gaussian')
+            #result_logistic = test_glm(n=n, p=p, s=s, snr = snr_grid[i], loss='logistic')
+            if result_lasso is not None:
+                covered, ci_length, pivots, naive_covered, naive_pvals = result_lasso[1]
+                lasso_selective.append(np.true_divide(covered.sum(), covered.shape[0]))
+                lasso_naive.append(np.true_divide(naive_covered.sum(), naive_covered.shape[0]))
+
+            #if result_logistic is not None:
+            #    covered, ci_length, pivots, naive_covered, naive_pvals = result_logistic[1]
+            #    logistic_selective.append(np.true_divide(covered.sum(), covered.shape[0]))
+            #    logistic_naive.append(np.true_divide(naive_covered.sum(), naive_covered.shape[0]))
+
+        lasso_selective_xaxis.append(np.mean(lasso_selective))
+        lasso_naive_xaxis.append(np.mean(lasso_naive))
+        #logistic_selective_xaxis.append(np.mean(logistic_selective))
+        #logistic_naive_xaxis.append(np.mean(logistic_selective))
+
+    return snr_grid, lasso_selective_xaxis, lasso_naive_xaxis, logistic_selective_xaxis, logistic_naive_xaxis
+
+
+
+if __name__=='__main__':
+    snr_grid, lasso_selective_xaxis, lasso_naive_xaxis, logistic_selective_xaxis, logistic_naive_xaxis = compute_coverage()
+
+    plt.plot(snr_grid, lasso_selective_xaxis, label = 'Lasso selective')
+    plt.plot(snr_grid, lasso_naive_xaxis, label = "Lasso naive")
+    #plt.plot(snr_grid, logistic_selective_xaxis, label = 'Logistic selective')
+    #plt.plot(snr_grid, logistic_naive_xaxis, label = "Logistic naive")
+    plt.legend(loc='lower right')
+    plt.xlabel("coverage")
+    plt.ylabel("snr")
+    print(lasso_selective_xaxis)
+    print(lasso_naive_xaxis)
+    #print(logistic_selective_xaxis)
+    #print(logistic_naive_xaxis)
+    plt.savefig('lasso_coverage.pdf')
+
+
+
+
--- selection/approx_ci/tests/test_glm.py
+++ selection/approx_ci/tests/test_glm.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import numpy as np
-import time
 import regreg.api as rr
 import selection.tests.reports as reports
 from selection.tests.instance import logistic_instance, gaussian_instance
@@ -16,26 +15,28 @@ from selection.randomized.query import naive_pvalues
 @register_report(['cover', 'ci_length', 'truth', 'naive_cover', 'naive_pvalues'])
 @set_sampling_params_iftrue(SMALL_SAMPLES, ndraw=10, burnin=10)
 @wait_for_return_value()
-def test_glm(n=200,
-             p=10,
-             s=3,
-             snr=5,
-             rho=0.1,
-             lam_frac = 1.2,
+def test_glm(n=500,
+             p=100,
+             s=5,
+             snr=3,
+             rho=0.,
+             lam_frac = 1.1,
              loss='gaussian',
              randomizer='gaussian'):
 
     from selection.api import randomization
-
+    if snr == 0:
+        s=0
     if loss == "gaussian":
         X, y, beta, nonzero, sigma = gaussian_instance(n=n, p=p, s=s, rho=rho, snr=snr, sigma=1.)
         lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.standard_normal((n, 2000)))).max(0)) * sigma
         loss = rr.glm.gaussian(X, y)
     elif loss == "logistic":
-        X, y, beta, _ = logistic_instance(n=n, p=p, s=s, rho=rho, snr=snr)
+        X, y, beta, nonzero = logistic_instance(n=n, p=p, s=s, rho=rho, snr=snr)
         loss = rr.glm.logistic(X, y)
         lam = lam_frac * np.mean(np.fabs(np.dot(X.T, np.random.binomial(1, 1. / 2, (n, 10000)))).max(0))
 
+    true_support = nonzero
     epsilon = 1. / np.sqrt(n)
 
     W = np.ones(p) * lam
@@ -48,23 +49,17 @@ def test_glm(n=200,
 
     M_est = M_estimator_approx(loss, epsilon, penalty, randomization, randomizer)
     M_est.solve_approx()
-    ci = approximate_conditional_density(M_est)
-    ci.solve_approx()
-
     active = M_est._overall
     active_set = np.asarray([i for i in range(p) if active[i]])
-
-    true_support = np.asarray([i for i in range(p) if i < s])
-
     nactive = np.sum(active)
 
     print("active set, true_support", active_set, true_support)
-
     true_vec = beta[active]
-
-    print("true coefficients", true_vec)
+    #print("true coefficients", true_vec)
 
     if (set(active_set).intersection(set(true_support)) == set(true_support))== True:
+        ci = approximate_conditional_density(M_est)
+        ci.solve_approx()
 
         ci_active = np.zeros((nactive, 2))
         covered = np.zeros(nactive, np.bool)
@@ -80,7 +75,6 @@ def test_glm(n=200,
         ci_naive = naive_confidence_intervals(target, M_est.target_observed)
         naive_pvals = naive_pvalues(target, M_est.target_observed, true_vec)
         naive_covered = np.zeros(nactive)
-        toc = time.time()
 
         for j in range(nactive):
             ci_active[j, :] = np.array(ci.approximate_ci(j))
@@ -94,16 +88,13 @@ def test_glm(n=200,
             if (ci_naive[j,0]<=true_vec[j]) and (ci_naive[j,1]>=true_vec[j]):
                 naive_covered[j]+=1
 
-        tic = time.time()
-        print('ci time now', tic - toc)
-
         return covered, ci_length, pivots, naive_covered, naive_pvals
     #else:
     #    return 0
 
 def report(niter=50, **kwargs):
 
-    kwargs = {'s': 0, 'n': 500, 'p': 100, 'snr': 7, 'loss': 'gaussian', 'randomizer':'gaussian'}
+    kwargs = {'s': 0, 'n': 500, 'p': 10, 'snr': 5, 'loss': 'gaussian', 'randomizer':'gaussian'}
     split_report = reports.reports['test_glm']
     screened_results = reports.collect_multiple_runs(split_report['test'],
                                                      split_report['columns'],
