a999a517e512a967a6bdb5f1434efd8ce2939714
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-02-14 15:58:03 -0800

    removing bayesian / approx_ci

--- selection/approx_ci/api.py
+++
@@ -1,6 +0,0 @@
-from .estimator_approx import (M_estimator_approx,
-                               threshold_score_approx,
-                               greedy_score_step_approx)
-
-from .ci_via_approx_density import approximate_conditional_density
-from .ci_approx_greedy_step import approximate_conditional_density
